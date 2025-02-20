a17d6d1274aa84b9dd49c593545d77c17cf1cd0f
Onurcan-Aktas <aktasonurcan420@gmail.com>
2025-02-20 23:27:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaselogin/SignUpActivity.kt
@@ -0,0 +1,50 @@
+package com.example.firebaselogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firebaselogin.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth:FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+
+        binding.signupButton.setOnClickListener {
+            val email=binding.signupEmail.text.toString()
+            val password=binding.signupPassword.text.toString()
+
+
+            if (email.isNotEmpty()&&password.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){task->
+                        if(task.isSuccessful){
+                            Toast.makeText(this,"Signup Succesful!",Toast.LENGTH_LONG).show()
+                            val intent=Intent(this,LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this,"Signup Unseccesful!",Toast.LENGTH_LONG).show()
+
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Please enter valid E-Mail and Password!",Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+    }
+}
